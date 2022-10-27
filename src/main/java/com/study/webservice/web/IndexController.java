9e7dad288adf29a2cc77ea8aa5366d212fb34b97
kingmin <rudals5569@gmail.com>
2022-10-28 03:56:44 +0900

    OAuth2를 이용한 구글 로그인 적용 ~194P

--- src/main/java/com/study/webservice/web/IndexController.java
+++ src/main/java/com/study/webservice/web/IndexController.java
@@ -1,24 +1,31 @@
 package com.study.webservice.web;
 
-import com.study.webservice.domain.posts.Posts;
+import com.study.webservice.config.auth.dto.SessionUser;
+
 import com.study.webservice.service.PostsService;
 import com.study.webservice.web.dto.PostsResponseDto;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("posts",postsService.findAllDesc());
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
