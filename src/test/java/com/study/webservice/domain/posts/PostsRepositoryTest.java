eed76884a7eecc85210d76b4861d1904d48026db
kingmin <rudals5569@gmail.com>
2022-11-02 18:37:11 +0900

    안 쓰는 import 삭제

--- src/test/java/com/study/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/study/webservice/domain/posts/PostsRepositoryTest.java
@@ -2,7 +2,6 @@ package com.study.webservice.domain.posts;
 
 
 
-import javafx.geometry.Pos;
 import org.junit.jupiter.api.AfterEach;
 
 import org.junit.jupiter.api.Test;
@@ -10,7 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
